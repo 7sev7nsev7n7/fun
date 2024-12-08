e6c9dc8b5694a8e674481b67fc0c68b305b845d1
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2024-12-07 22:10:12 -0600

    Added new program, mimicking the functionality of 'cat'

---
+++ src/catnew.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) { // main function
+	// iterate over arguments in **argv
+	for (int i=1; i<argc; i++) {
+		FILE *file;
+		if (file = fopen(*(argv+i), "r")) { // if file exists, read data
+			file = fopen(*(argv+i), "r");
+			char ch;
+			while ((ch = fgetc(file)) != EOF) {
+				putchar(ch);
+			}
+			fclose(file); // close file stream before ending loop
+		} else { // else print error message
+			fprintf(stderr, "catnew: %s: No such file or directory\n", *(argv+i));
+		}
+	}
+}
